c0120d94f4ea1d1e99c85130773966a2136ba2ad
Sypztep <chainot@hotmail.co.th>
2024-01-22 19:51:00 +0700

    How I forgot client one

---
+++ src/main/java/sypztep/mamy/moonay/client/MoonayClient.java
@@ -0,0 +1,10 @@
+package sypztep.mamy.moonay.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoonayClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
