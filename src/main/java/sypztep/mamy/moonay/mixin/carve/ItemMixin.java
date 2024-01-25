7d9a54767746d64904730ddaf92595906fb01385
Sypztep <chainot@hotmail.co.th>
2024-01-26 01:35:48 +0700

    Move some method to Helper

--- src/main/java/sypztep/mamy/moonay/mixin/carve/ItemMixin.java
+++ src/main/java/sypztep/mamy/moonay/mixin/carve/ItemMixin.java
@@ -1,7 +1,5 @@
 package sypztep.mamy.moonay.mixin.carve;
 
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
@@ -11,54 +9,39 @@ import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import sypztep.mamy.moonay.common.util.SpecialEnchantment;
+import sypztep.mamy.moonay.common.util.MoonayHelper;
 
 @Mixin(Item.class)
 public class ItemMixin {
     @Inject(method = "finishUsing", at = @At("HEAD"))
     private void finishUsing(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable<ItemStack> cir) {
-        if (hasSpecialEnchantment(stack)) {
-            getSpecialEnchantment(stack).onFinishUsing(stack,world,user);
+        if (MoonayHelper.hasSpecialEnchantment(stack)) {
+            MoonayHelper.getSpecialEnchantment(stack).onFinishUsing(stack,world,user);
         }
     }
 
     @Inject(method = "getMaxUseTime", at = @At("HEAD"), cancellable = true)
     private void getMaxUseTime(ItemStack stack, CallbackInfoReturnable<Integer> cir) {
-        if (hasSpecialEnchantment(stack)) {
-            cir.setReturnValue(getSpecialEnchantment(stack).getMaxUseTime(stack));
+        if (MoonayHelper.hasSpecialEnchantment(stack)) {
+            cir.setReturnValue(MoonayHelper.getSpecialEnchantment(stack).maxUseTime(stack));
         }
     }
 
     @Inject(method = "getUseAction", at = @At("HEAD"), cancellable = true)
     private void getUseAction(ItemStack stack, CallbackInfoReturnable<UseAction> cir) {
-        if (hasSpecialEnchantment(stack)) {
-            cir.setReturnValue(getSpecialEnchantment(stack).getUseAction(stack));
+        if (MoonayHelper.hasSpecialEnchantment(stack)) {
+            cir.setReturnValue(MoonayHelper.getSpecialEnchantment(stack).useAction(stack));
         }
     }
 
     @Inject(method = "use", at = @At("HEAD"), cancellable = true)
     private void use(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
         ItemStack stack = user.getStackInHand(hand);
-        if (hasSpecialEnchantment(stack)) {
-            cir.setReturnValue(getSpecialEnchantment(stack).onUse(world, user, hand, stack));
+        if (MoonayHelper.hasSpecialEnchantment(stack)) {
+            cir.setReturnValue(MoonayHelper.getSpecialEnchantment(stack).onUse(world, user, hand, stack));
         }
-    }
-    @Unique
-    private boolean hasSpecialEnchantment(ItemStack stack) {
-        return getSpecialEnchantment(stack) != null;
-    }
-
-    @Unique
-    private SpecialEnchantment getSpecialEnchantment(ItemStack stack) {
-        for (Enchantment enchantment : EnchantmentHelper.get(stack).keySet()) {
-            if (enchantment instanceof SpecialEnchantment) {
-                return (SpecialEnchantment) enchantment;
-            }
-        }
-        return null;
     }
 }
