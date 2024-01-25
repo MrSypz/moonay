6983b634e20f8a2a4548bd28a3fa27ca083b33c8
Sypztep <chainot@hotmail.co.th>
2024-01-26 03:16:27 +0700

    Check Item Method

--- src/main/java/sypztep/mamy/moonay/common/enchantment/StigmaEnchantment.java
+++ src/main/java/sypztep/mamy/moonay/common/enchantment/StigmaEnchantment.java
@@ -2,7 +2,6 @@ package sypztep.mamy.moonay.common.enchantment;
 
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentTarget;
-import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
@@ -14,12 +13,15 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
-import sypztep.mamy.moonay.common.MoonayMod;
 import sypztep.mamy.moonay.common.init.ModEnchantments;
+import sypztep.mamy.moonay.common.init.ModParticles;
 import sypztep.mamy.moonay.common.init.ModStatusEffects;
+import sypztep.mamy.moonay.common.packetc2s.StigmaPacket;
 import sypztep.mamy.moonay.common.util.AbilityHelper;
 import sypztep.mamy.moonay.common.util.MoonayHelper;
 import sypztep.mamy.moonay.common.util.SpecialEnchantment;
+
+import static sypztep.mamy.moonay.common.util.MoonayHelper.checkIsItemCorrectUse;
 
 public class StigmaEnchantment extends AxeEnchantment implements SpecialEnchantment {
     public StigmaEnchantment(Rarity weight, EnchantmentTarget target, EquipmentSlot... slotTypes) {
@@ -42,9 +44,14 @@ public class StigmaEnchantment extends AxeEnchantment implements SpecialEnchantm
         int lvl = MoonayHelper.getEntLvl(this, stack);
         double value = living.getAttributeValue(EntityAttributes.GENERIC_ATTACK_DAMAGE);
         AbilityHelper.boxDamage(living,living.getWorld().getDamageSources().playerAttack((PlayerEntity) living),3, (float) value * 1.5f); //150% Damage base on player attack damage
-        living.heal(((float) value * 0.2f + AbilityHelper.getMissingHealth(living,0.12f)) * AbilityHelper.getHitAmount());
+        if (living.getWorld().isClient())
+            StigmaPacket.send();
+        stigmaParticle(living);
+        living.heal(((float) value * 0.25f + AbilityHelper.getMissingHealth(living,0.12f)) * AbilityHelper.getHitAmount());
         living.addStatusEffect(new StatusEffectInstance(ModStatusEffects.STIGMA_COOLDOWN, 600 - (lvl * 20)));
-        MoonayMod.LOGGER.info(String.valueOf(value * 0.2f + AbilityHelper.getMissingHealth(living,0.12f) * AbilityHelper.getHitAmount()));
+    }
+    public static void stigmaParticle(Entity entity) {
+        entity.getWorld().addParticle(ModParticles.BLOODWAVE, entity.getX(), entity.getY() + 0.1, entity.getZ(), 0,0,0);
     }
 
     @Override
@@ -59,10 +66,12 @@ public class StigmaEnchantment extends AxeEnchantment implements SpecialEnchantm
 
     @Override
     public TypedActionResult<ItemStack> onUse(World world, PlayerEntity user, Hand hand, ItemStack stack) {
-        if (MoonayHelper.hasEnt(this, stack) && MoonayHelper.dontHasThisStatusEffect(ModStatusEffects.STIGMA_COOLDOWN, user)) {
+        MoonayHelper.WeaponType weaponType = checkIsItemCorrectUse(user, stack);
+        if (MoonayHelper.hasEnt(this, stack) && MoonayHelper.dontHasThisStatusEffect(ModStatusEffects.STIGMA_COOLDOWN, user) && weaponType == MoonayHelper.WeaponType.AXE) {
             user.setCurrentHand(hand);
             return TypedActionResult.consume(stack);
-        }
+        } else if (weaponType == MoonayHelper.WeaponType.SWORD)
+            return TypedActionResult.pass(stack);
         return TypedActionResult.pass(stack);
     }
 }
