c237b4f637446288e439d2c35ef0ef8932e47061
Sypztep <chainot@hotmail.co.th>
2024-01-26 03:14:45 +0700

    Make particle and function check

--- src/main/java/sypztep/mamy/moonay/common/enchantment/CarveEnchantment.java
+++ src/main/java/sypztep/mamy/moonay/common/enchantment/CarveEnchantment.java
@@ -9,18 +9,18 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.SwordItem;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
-import sypztep.mamy.moonay.common.MoonayMod;
-import sypztep.mamy.moonay.common.init.ModEnchantments;
 import sypztep.mamy.moonay.common.init.ModStatusEffects;
 import sypztep.mamy.moonay.common.packetc2s.CarveSoulPacket;
 import sypztep.mamy.moonay.common.util.AbilityHelper;
 import sypztep.mamy.moonay.common.util.MoonayHelper;
 import sypztep.mamy.moonay.common.util.SpecialEnchantment;
+
+import static sypztep.mamy.moonay.common.util.MoonayHelper.checkIsItemCorrectUse;
 
 public class CarveEnchantment extends OnHitApplyEnchantment implements SpecialEnchantment {
     public CarveEnchantment(Rarity weight, EnchantmentTarget target, EquipmentSlot... slotTypes) {
@@ -45,9 +45,17 @@ public class CarveEnchantment extends OnHitApplyEnchantment implements SpecialEn
             living.heal(j + AbilityHelper.getMissingHealth(living,0.05f));
             if (living.getWorld().isClient())
                 CarveSoulPacket.send();
-            MoonayHelper.carvesoulParticle(living);
+            carvesoulParticle(living);
             living.removeStatusEffect(ModStatusEffects.STALWART);
             living.addStatusEffect(new StatusEffectInstance(ModStatusEffects.STALWART_COOLDOWN, 240 - (lvl * 2)));
+        }
+    }
+    public static void carvesoulParticle(Entity entity) {
+        for (int i = 0; i <= 360; i += 8) {
+            double circle = Math.toRadians(i);
+            double x = 3 * Math.cos(circle) * 1.5;
+            double z = 3* Math.sin(circle) * 1.5;
+            entity.getWorld().addParticle(ParticleTypes.COMPOSTER, entity.getX() + x, entity.getEyeY(), entity.getZ() + z, 0,0,0);
         }
     }
 
@@ -63,10 +71,12 @@ public class CarveEnchantment extends OnHitApplyEnchantment implements SpecialEn
 
     @Override
     public TypedActionResult<ItemStack> onUse(World world, PlayerEntity user, Hand hand, ItemStack stack) {
-        if (MoonayHelper.hasEnt(this, stack) && MoonayHelper.dontHasThisStatusEffect(ModStatusEffects.STALWART_COOLDOWN, user)) {
+        MoonayHelper.WeaponType weaponType = checkIsItemCorrectUse(user, stack);
+        if (MoonayHelper.hasEnt(this, stack) && MoonayHelper.dontHasThisStatusEffect(ModStatusEffects.STALWART_COOLDOWN, user) && weaponType == MoonayHelper.WeaponType.SWORD) {
             user.setCurrentHand(hand);
             return TypedActionResult.consume(stack);
-        }
+        } else if (weaponType == MoonayHelper.WeaponType.AXE)
+            return TypedActionResult.pass(stack);
         return TypedActionResult.pass(stack);
     }
 }
