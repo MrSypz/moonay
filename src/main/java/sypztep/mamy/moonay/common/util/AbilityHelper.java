e8a4e230892e6cf90e699302231d105474b3d2f7
Sypztep <chainot@hotmail.co.th>
2024-01-27 05:56:07 +0700

    Remove unuse import

--- src/main/java/sypztep/mamy/moonay/common/util/AbilityHelper.java
+++ src/main/java/sypztep/mamy/moonay/common/util/AbilityHelper.java
@@ -2,9 +2,6 @@ package sypztep.mamy.moonay.common.util;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.world.World;
-import sypztep.mamy.moonay.common.init.ModDamageTypes;
 
 import java.util.List;
 
