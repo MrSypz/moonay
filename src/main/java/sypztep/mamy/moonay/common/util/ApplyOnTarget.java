e106e2e98ca4cf542e03ddd86499934c65f27e66
Sypztep <chainot@hotmail.co.th>
2024-01-31 10:45:18 +0700

    Interface for handle ApplyOnTarget and ApplyOnUser

---
+++ src/main/java/sypztep/mamy/moonay/common/util/ApplyOnTarget.java
@@ -0,0 +1,7 @@
+package sypztep.mamy.moonay.common.util;
+
+import net.minecraft.entity.LivingEntity;
+
+public interface ApplyOnTarget {
+    void apply(LivingEntity target);
+}
---
+++ src/main/java/sypztep/mamy/moonay/common/util/ApplyOnUser.java
@@ -0,0 +1,7 @@
+package sypztep.mamy.moonay.common.util;
+
+import net.minecraft.entity.LivingEntity;
+
+public interface ApplyOnUser {
+    void apply(LivingEntity user);
+}
