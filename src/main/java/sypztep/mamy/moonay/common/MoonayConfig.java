b240ce36d2c55d2e448682e4ed8f7e5a9676764e
Sypztep <chainot@hotmail.co.th>
2024-01-31 12:31:51 +0700

    remove unuse config

--- src/main/java/sypztep/mamy/moonay/common/MoonayConfig.java
+++ src/main/java/sypztep/mamy/moonay/common/MoonayConfig.java
@@ -8,9 +8,8 @@ import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 @Config.Gui.Background("minecraft:textures/block/dirt.png")
 
 public class MoonayConfig implements ConfigData {
-    @Comment("How many perc that gonna stripe armor out Default 8% per stack")
+    @Comment("How many percent that gonna stripe armor out.")
     public double carveModifier = -0.08D;
+    @Comment("Movement speed multiply from stalwart.")
     public double stalwartSpeedModifier = 0.02D;
-    public float weaponCooldownProgess = 0.7f;
-    public boolean shouldcancelattack = false;
 }
