22cac175bbe8df624506bd8b282e296f18c406cd
Sypztep <chainot@hotmail.co.th>
2024-01-31 12:29:12 +0700

    Remove

--- src/main/java/sypztep/mamy/moonay/common/util/ApplyOnTarget.java
+++
@@ -1,7 +0,0 @@
-package sypztep.mamy.moonay.common.util;
-
-import net.minecraft.entity.LivingEntity;
-
-public interface ApplyOnTarget {
-    void apply(LivingEntity target);
-}
--- src/main/java/sypztep/mamy/moonay/common/util/ApplyOnUser.java
+++
@@ -1,7 +0,0 @@
-package sypztep.mamy.moonay.common.util;
-
-import net.minecraft.entity.LivingEntity;
-
-public interface ApplyOnUser {
-    void apply(LivingEntity user);
-}
