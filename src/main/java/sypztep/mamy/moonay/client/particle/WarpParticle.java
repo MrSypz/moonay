e34eede14b94745dcc1ecf4bdf549ed88e189d3e
Sypztep <chainot@hotmail.co.th>
2024-01-31 10:45:47 +0700

    Smooth!

--- src/main/java/sypztep/mamy/moonay/client/particle/WarpParticle.java
+++ src/main/java/sypztep/mamy/moonay/client/particle/WarpParticle.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.particle.*;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.particle.DefaultParticleType;
+import net.minecraft.particle.ParticleTypes;
 
 @Environment(EnvType.CLIENT)
 public class WarpParticle extends SpriteBillboardParticle {
@@ -12,11 +13,11 @@ public class WarpParticle extends SpriteBillboardParticle {
     WarpParticle(ClientWorld world, double x, double y, double z, SpriteProvider spriteProvider, double velocityX, double velocityY, double velocityZ) {
         super(world, x, y , z, 0.0, 0.0, 0.0);
         this.spriteProvider = spriteProvider;
-        this.maxAge = 6;
+        this.maxAge = 8;
         this.velocityX = velocityX;
         this.velocityY = velocityY;
         this.velocityZ = velocityZ;
-        this.scale = 1f;
+        this.scale = 0.85f;
         this.gravityStrength = 0F;
         this.setSpriteForAge(this.spriteProvider);
     }
@@ -25,12 +26,19 @@ public class WarpParticle extends SpriteBillboardParticle {
         super.tick();
         this.setSpriteForAge(this.spriteProvider);
 
-        if(this.age++ >= this.maxAge || this.scale <= 0 || this.alpha <= 0)  //Despawns the particle if the age has reached the max age, or if the scale is 0, or if the alpha is 0
+        if(this.age++ >= this.maxAge || this.scale <= 0 || this.alpha <= 0) //Despawns the particle if the age has reached the max age, or if the scale is 0, or if the alpha is 0
             this.markDead(); //Despawns the particle
-        else
+
+        else {
+            this.scale += 0.1f;
             this.alpha -= 0.2f; //Slowly fades away upon hitting the ground
+        }
     }
 
+    @Override
+    protected int getBrightness(float tint) {
+        return 255;
+    }
 
     @Override
     public ParticleTextureSheet getType() {
