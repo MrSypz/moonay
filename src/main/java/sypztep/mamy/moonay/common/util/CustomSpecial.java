d222b1b5e298c0dc65646b0fe8179601e953f94d
Sypztep <chainot@hotmail.co.th>
2024-02-05 18:45:22 +0700

    add param for easy to code no need to int getlevel anymore!!!!!!!!

--- src/main/java/sypztep/mamy/moonay/common/util/CustomSpecial.java
+++ src/main/java/sypztep/mamy/moonay/common/util/CustomSpecial.java
@@ -1,9 +1,11 @@
 package sypztep.mamy.moonay.common.util;
 
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 
 public interface CustomSpecial {
-    void applyOnTarget(LivingEntity user, Entity target);
-    void applyOnUser(LivingEntity user);
+    void applyOnTarget(LivingEntity user, Entity target, int level);
+    void applyOnUser(LivingEntity user, int level);
+    Enchantment getEnchantment();
 }
