9bfd03975da960947863e8b87d243ffe103dd91a
Sypztep <chainot@hotmail.co.th>
2024-02-05 21:52:57 +0700

    iden Hudcallback

--- src/main/java/sypztep/mamy/moonay/client/MoonayClient.java
+++ src/main/java/sypztep/mamy/moonay/client/MoonayClient.java
@@ -6,7 +6,7 @@ import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import sypztep.mamy.moonay.client.event.CarveRenderEvent;
+import sypztep.mamy.moonay.client.event.HealthBarRenderEvent;
 import sypztep.mamy.moonay.client.packets2c.AddCarveSoulParticlePacket;
 import sypztep.mamy.moonay.client.packets2c.AddStigmaParticlePacket;
 import sypztep.mamy.moonay.client.packets2c.ConfigSyncPacket;
@@ -14,6 +14,7 @@ import sypztep.mamy.moonay.client.packets2c.SyncCritS2CPacket;
 import sypztep.mamy.moonay.client.particle.BloodwaveParticle;
 import sypztep.mamy.moonay.client.particle.ShockwaveParticle;
 import sypztep.mamy.moonay.client.particle.WarpParticle;
+import sypztep.mamy.moonay.common.init.ModConfig;
 import sypztep.mamy.moonay.common.init.ModEnchantments;
 import sypztep.mamy.moonay.common.init.ModParticles;
 import sypztep.mamy.moonay.common.util.AbilityHelper;
@@ -35,12 +36,12 @@ public class MoonayClient implements ClientModInitializer {
         particleRegistry.register(ModParticles.BLOODWAVE, BloodwaveParticle.Factory::new);
         particleRegistry.register(ModParticles.WARP, WarpParticle.Factory::new);
 
-        HudRenderCallback.EVENT.register(new CarveRenderEvent());
+        HudRenderCallback.EVENT.register(new HealthBarRenderEvent());
 
         ClientTickEvents.END_CLIENT_TICK.register(minecraft -> {
             if (minecraft.player != null) {
                 if (MoonayHelper.hasEnchantment(ModEnchantments.STIGMA,minecraft.player.getMainHandStack()))
-                    AbilityHelper.boxArea(minecraft.player, 3);
+                    AbilityHelper.boxArea(minecraft.player, ModConfig.CONFIG.stigmarange);
             }
         });
     }
