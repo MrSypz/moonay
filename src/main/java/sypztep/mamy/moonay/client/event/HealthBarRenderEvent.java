06779849b429d146789f7a39e2208af6a540e671
Sypztep <chainot@hotmail.co.th>
2024-02-05 21:53:02 +0700

    Rename

--- src/main/java/sypztep/mamy/moonay/client/event/HealthBarRenderEvent.java
+++ src/main/java/sypztep/mamy/moonay/client/event/HealthBarRenderEvent.java
@@ -12,8 +12,7 @@ import sypztep.mamy.moonay.mixin.enchantment.special.InGameHudAccessor;
 
 import static sypztep.mamy.moonay.common.init.ModConfig.CONFIG;
 
-public class CarveRenderEvent implements HudRenderCallback {
-    public static int forcedHeight = -1;
+public class HealthBarRenderEvent implements HudRenderCallback {
 
     @Override
     public void onHudRender(DrawContext drawContext, float tickDelta) {
@@ -22,15 +21,18 @@ public class CarveRenderEvent implements HudRenderCallback {
 
 
         if (client != null) {
+            int scaledWidth = minecraft.getWindow().getScaledWidth(), scaledHeight = minecraft.getWindow().getScaledHeight();
+
             if (MoonayHelper.hasEnchantment(ModEnchantments.CARVE,client.getMainHandStack())) {
-                int scaledWidth = minecraft.getWindow().getScaledWidth(), scaledHeight = minecraft.getWindow().getScaledHeight();
                 if (MoonayHelper.hasStatusWithAmpValue$greather(ModStatusEffects.STALWART,client,0)) {
 //                    MoonayHelper.drawtextcustom(drawContext, MinecraftClient.getInstance().textRenderer, "Healing Bonus : " + (MoonayHelper.getStatusAmp(ModStatusEffects.STALWART, client) + AbilityHelper.getMissingHealth(client, 0.05f)),
 //                            (scaledWidth / 2) - 25, (scaledHeight / 2) + 6, 0xffffff, 0, false);
-                    forcedHeight = (scaledHeight / 2) + 6;
-                 ((InGameHudAccessor) minecraft.inGameHud).moonay$renderHealthBar(drawContext, minecraft.player, (scaledWidth / 2) - 25,forcedHeight , 1, -1, (float) Math.ceil(MoonayHelper.getStatusAmp(ModStatusEffects.STALWART, client) + AbilityHelper.getMissingHealth(client, 0.5f)), (int) (MoonayHelper.getStatusAmp(ModStatusEffects.STALWART, client) + AbilityHelper.getMissingHealth(client, CONFIG.carvehealratio)), (int) (MoonayHelper.getStatusAmp(ModStatusEffects.STALWART, client) + AbilityHelper.getMissingHealth(client, CONFIG.carvehealratio)), 0, false);
-                    forcedHeight = -1;
+                 ((InGameHudAccessor) minecraft.inGameHud).moonay$renderHealthBar(drawContext, minecraft.player, (scaledWidth / 2) - 25,(scaledHeight / 2) + 25 , 1, -1, (float) Math.ceil(MoonayHelper.getStatusAmp(ModStatusEffects.STALWART, client) + AbilityHelper.getMissingHealth(client, 0.5f)), (int) (MoonayHelper.getStatusAmp(ModStatusEffects.STALWART, client) + AbilityHelper.getMissingHealth(client, CONFIG.carvehealratio)), (int) (MoonayHelper.getStatusAmp(ModStatusEffects.STALWART, client) + AbilityHelper.getMissingHealth(client, CONFIG.carvehealratio)), 0, false);
                 }
+            }
+            if (MoonayHelper.hasEnchantment(ModEnchantments.STIGMA,client.getActiveItem())) {
+                double amount = (0.25f + AbilityHelper.getMissingHealth(client, 0.12f)) * AbilityHelper.getHitAmount();
+                ((InGameHudAccessor) minecraft.inGameHud).moonay$renderHealthBar(drawContext, minecraft.player, (scaledWidth / 2) - 25,(scaledHeight / 2) + 25 , 1, -1, client.getMaxHealth(), (int) amount, (int) amount ,0,true);
             }
         }
     }
