dfcebabbaa6670c2fe24b3b9e13a630e97f9a8fa
Sypztep <chainot@hotmail.co.th>
2024-02-05 18:45:48 +0700

    What delete a space?

--- src/main/java/sypztep/mamy/moonay/common/data/CritOverhaulData.java
+++ src/main/java/sypztep/mamy/moonay/common/data/CritOverhaulData.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 public class CritOverhaulData {
     private final Map<String, CritOverhaulEntry> items = new HashMap<>();
-
     public Map<String, CritOverhaulEntry> getItems() {
         return items;
     }
