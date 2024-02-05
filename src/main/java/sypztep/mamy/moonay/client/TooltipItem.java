53cdb2e44073fe62de02f6870f1128e2e603a72f
Sypztep <chainot@hotmail.co.th>
2024-02-05 18:43:12 +0700

    Remove unused for now.

--- src/main/java/sypztep/mamy/moonay/client/TooltipItem.java
+++ src/main/java/sypztep/mamy/moonay/client/TooltipItem.java
@@ -14,6 +14,7 @@ import org.apache.commons.lang3.mutable.MutableFloat;
 import sypztep.mamy.moonay.common.MoonayMod;
 import sypztep.mamy.moonay.common.data.CritOverhaulConfig;
 import sypztep.mamy.moonay.common.data.CritOverhaulEntry;
+import sypztep.mamy.moonay.common.init.ModConfig;
 import sypztep.mamy.moonay.common.init.ModEnchantments;
 import sypztep.mamy.moonay.common.init.ModStatusEffects;
 import sypztep.mamy.moonay.common.util.AbilityHelper;
@@ -28,16 +29,16 @@ public class TooltipItem {
     public static void onTooltipRender(ItemStack stack, List<Text> lines, TooltipContext context) {
         ClientPlayerEntity player = MinecraftClient.getInstance().player;
         String itemId = Registries.ITEM.getId(stack.getItem()).toString();
-        if (MoonayHelper.hasEnt(ModEnchantments.CARVE, stack)) {
+//        if (CRIT_OVERHAUL_LOAD_CONFIG.getCritDataForItem(itemId).isValid()) {
+//            if (CRIT_OVERHAUL_LOAD_CONFIG.getCritDataForItem(itemId).getCritChance() >= 0) {
+//                addCritOverhaulTooltip(stack, lines, Formatting.DARK_GREEN);
+//            } else if (CRIT_OVERHAUL_LOAD_CONFIG.getCritDataForItem(itemId).getCritChance() < 0)
+//                addCritOverhaulTooltip(stack, lines, Formatting.RED);
+//        }
+        if (MoonayHelper.hasEnchantment(ModEnchantments.CARVE, stack)) {
             addCarveTooltip(lines, player);
-        } else if (MoonayHelper.hasEnt(ModEnchantments.STIGMA, stack)) {
+        } else if (MoonayHelper.hasEnchantment(ModEnchantments.STIGMA, stack)) {
             addStigmaTooltip(lines, player);
-        }
-        if (CRIT_OVERHAUL_LOAD_CONFIG.getCritDataForItem(itemId).isValid()) {
-            if (CRIT_OVERHAUL_LOAD_CONFIG.getCritDataForItem(itemId).getCritChance() >= 0) {
-                addCritOverhaulTooltip(stack, lines, Formatting.DARK_GREEN);
-            } else if (CRIT_OVERHAUL_LOAD_CONFIG.getCritDataForItem(itemId).getCritChance() < 0)
-                addCritOverhaulTooltip(stack, lines, Formatting.RED);
         }
     }
     private static void addCritOverhaulTooltip(ItemStack stack, List<Text> lines,Formatting color) {
@@ -51,8 +52,8 @@ public class TooltipItem {
 
     private static void addCarveTooltip(List<Text> lines, ClientPlayerEntity client) {
         if (client != null) {
-            double amount = MoonayHelper.getStatusAmp(ModStatusEffects.STALWART, client) + AbilityHelper.getMissingHealth(client, 0.05f);
-            addFormattedTooltip(lines, amount, "carve");
+            double amount = MoonayHelper.getStatusAmp(ModStatusEffects.STALWART, client) + AbilityHelper.getMissingHealth(client, ModConfig.CONFIG.carvehealratio);
+            addFormattedTooltip(lines, String.valueOf(amount), "carve");
         }
     }
 
@@ -60,22 +61,22 @@ public class TooltipItem {
         if (client != null) {
             double damage = client.getAttributeValue(EntityAttributes.GENERIC_ATTACK_DAMAGE);
             double amount = damage * 0.25f + AbilityHelper.getMissingHealth(client, 0.12f);
-            addFormattedTooltip2(lines, amount, " + " + AbilityHelper.getHitAmount(), "carve", "stigma.desc");
-            addFormattedTooltip(lines, 1f + MoonayHelper.getEntLvl(ModEnchantments.STIGMA,client.getMainHandStack()), "stigma.passive", "stigma.desc.passive");
+            addFormattedTooltip(lines, amount + " + " + AbilityHelper.getHitAmount(), "stigma","stigma.condition");
+            addFormattedTooltip(lines, 1f + " + " + MoonayHelper.getEnchantmentLvl(ModEnchantments.STIGMA, client.getMainHandStack()), "stigma.desc","stigma.desc2");
         }
     }
-    private static void addFormattedTooltip(List<Text> lines, double amount, String key, String... extraKeys) {
-        String formattedAmount = String.format("%.2f", amount);
-        MutableFloat mutableFloat = new MutableFloat(formattedAmount);
 
+    private static void addFormattedTooltip(List<Text> lines, String value, String key, String... extraKeys) {
         Text tooltip = Text.translatable(MoonayMod.MODID + ".modifytooltip." + key).formatted(Formatting.GRAY)
-                .append(Text.literal(mutableFloat.toString()).formatted(Formatting.RED));
+                .append(Text.literal(value).formatted(Formatting.RED));
 
         for (String extraKey : extraKeys)
             tooltip = tooltip.copy().append(Text.translatable(MoonayMod.MODID + ".modifytooltip." + extraKey).formatted(Formatting.GRAY));
 
         lines.add(tooltip);
     }
+
+
     private static void addFormattedTooltip(List<Text> lines, double amount, String key,Formatting formatting, String... extraKeys) {
         String formattedAmount = String.format("%.2f", amount);
         MutableFloat mutableFloat = new MutableFloat(formattedAmount);
@@ -84,18 +85,6 @@ public class TooltipItem {
 
         for (String extraKey : extraKeys)
             tooltip = tooltip.copy().append(Text.translatable(MoonayMod.MODID + ".modifytooltip." + extraKey).formatted(Formatting.DARK_GREEN));
-
-        lines.add(tooltip);
-    }
-    private static void addFormattedTooltip2(List<Text> lines, double amount, String string, String key, String... extraKeys) {
-        String formattedAmount = String.format("%.2f", amount);
-        MutableFloat mutableFloat = new MutableFloat(formattedAmount);
-
-        Text tooltip = Text.translatable(MoonayMod.MODID + ".modifytooltip." + key).formatted(Formatting.GRAY)
-                .append(Text.literal(mutableFloat + string).formatted(Formatting.RED));
-
-        for (String extraKey : extraKeys)
-            tooltip = tooltip.copy().append(Text.translatable(MoonayMod.MODID + ".modifytooltip." + extraKey).formatted(Formatting.GRAY));
 
         lines.add(tooltip);
     }
